7b483125a9760f413577da25ca09ca8d0fbadc4e
Matt Ho <matt.ho@gmail.com>
2020-10-18 15:55:32 -0500

    renamed WithLogger to InjectLogger

--- http.go
+++ http.go
@@ -20,12 +20,13 @@ func WithFactory(fn func(req *http.Request) *zap.Logger) Option {
 	}
 }
 
-// WithLogger returns a specified logging
-func WithLogger(logger *zap.Logger) Option {
-	return func(o *options) {
-		o.makeLogger = func(req *http.Request) *zap.Logger {
-			return logger
-		}
+// InjectLogger returns handler to inject logger into request context
+func InjectLogger(logger *zap.Logger) func(http.Handler) http.Handler {
+	return func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			req = req.WithContext(NewContext(req.Context(), logger))
+			handler.ServeHTTP(w, req)
+		})
 	}
 }
 
